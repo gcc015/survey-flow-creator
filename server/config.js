58fc4b52f4a796b4a6330fa3f4f4e5f6437e1c1d
gcc015 <53164597+gcc015@users.noreply.github.com>
2025-04-13 15:07:31 +0800

    修改config.js  .env

--- server/config.js
+++ server/config.js
@@ -5,10 +5,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  DB_HOST: process.env.DB_HOST || '127.0.0.1',
-  DB_USER: process.env.DB_USER || 'root',
-  DB_PASSWORD: process.env.DB_PASSWORD || 'admin',
-  DB_DATABASE: process.env.DB_DATABASE || 'deep_survey',
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
   JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret_key_change_this_in_production'
 };
 
